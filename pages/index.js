948da6712466c61b3c22186e591d17617e9bdd7d
Devang <devangmanjramkar@gmail.com>
2022-10-08 16:18:26 +0530

    added more secrets

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,11 @@ import Plogin from './Plogin.js';
 import { getSession } from 'next-auth/react';
 const environment = process.env.NODE_ENV;
 
-export default function Home() {
-  const user = "S";
-  const role = undefined;
+export default function Home({ user }) {
+  const role = user?.role;
   return (
     <div className={styles.container}>
-      {role === undefined && <Plogin />}
+      {!user && <Plogin />}
       {user && role === 'User' && (
         <UserHome role={role} />
       )}
@@ -20,27 +19,26 @@ export default function Home() {
 }
 
 export async function getServerSideProps({ req }) {
-  // Fetch data from external API
 
-  // const session = await getSession({ req });
-  // const email = session?.user.email;
+  const session = await getSession({ req });
+  const email = session?.user.email;
 
-  // const url =
-  //   environment === 'production' ? 'https://semac.vercel.app/api' : `http://localhost:3000/api`;
+  const url =
+    environment === 'production' ? 'https://semac.vercel.app/api' : `http://localhost:3000/api`;
 
-  // let user = null;
+  let user = null;
 
-  // if (email) {
-  //   const res = await fetch(`${url}/auth/user/?email=${email}`, {
-  //     method: 'GET',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  //   user = await res.json();
+  if (email) {
+    const res = await fetch(`${url}/auth/user/?email=${email}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    user = await res.json();
 
-  let user = "User";
+    let user = "User";
 
-  // Pass data to the page via props
-  return { props: { user } };
-}
+    // Pass data to the page via props
+    return { props: { user } };
+  }
