157c6bb36824178862b3fdf96f729976f936e033
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-10-21 23:35:02 +0530

    started flask integration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 const withPWA = require('next-pwa')({
-  dest: 'public'
+    dest: 'public',
 });
 
 module.exports = withPWA({
-  images: {
-    domains: ["links.papareact.com", "fakestoreapi.com"]
-  }
+    async rewrites() {
+        return [
+            {
+                source: '/flask/hello/:path*',
+                destination: 'http://localhost:5000/hello/:path*',
+            },
+        ];
+    },
+
+    images: {
+        domains: ['links.papareact.com', 'fakestoreapi.com'],
+    },
 });
