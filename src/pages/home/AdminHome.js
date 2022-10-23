9a4ef786d643d952ba3b1edb6600e4ba40ddaab0
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-10-23 16:04:48 +0530

    Integrated Flask and NextJS

--- src/pages/home/AdminHome.js
+++ src/pages/home/AdminHome.js
@@ -2,38 +2,57 @@ import React from 'react';
 import styles from '../../../styles/main.module.css';
 
 const TeeacherHome = ({ role }) => {
-  if (role && role === 'Teacher') {
-    return (
-      <div className={(styles.contentcontainer, styles.tbg)}>
-        <section>
-          <div className={styles.contentwrapper}>
-            <div className={styles.tabs}>
-              <div className={styles.categories}>
-                <a href="/timetable/TimetableUpdater">
-                  <h2 className={styles.ttabs}>TIMETABLE UPDATER</h2>
-                </a>
-              </div>
+    // const handlePredictClick = (event) => {
+    //     //const proxyurl = "https://salty-reaches-05509.herokuapp.com/";
+    //     const url = 'http://127.0.0.1:5000/prediction/';
+    //     setIsloading(true);
+    //     fetch(url, {
+    //         headers: {
+    //             Accept: 'application/json',
+    //             'Content-Type': 'application/json',
+    //         },
+    //         method: 'POST',
+    //         body: JSON.stringify(formData),
+    //     }) //https://salty-reaches-05509.herokuapp.com/http://127.0.0.1:5000/prediction
+    //         .then((response) => response.json())
+    //         .then((response) => {
+    //             setResult(response.result);
+    //             setIsloading(false);
+    //         });
+    // };
+
+    if (role && role === 'Teacher') {
+        return (
+            <div className={(styles.contentcontainer, styles.tbg)}>
+                <section>
+                    <div className={styles.contentwrapper}>
+                        <div className={styles.tabs}>
+                            <div className={styles.categories}>
+                                <a href="/timetable/TimetableUpdater">
+                                    <h2 className={styles.ttabs}>TIMETABLE UPDATER</h2>
+                                </a>
+                            </div>
+                        </div>
+                        <div className={styles.tabs}>
+                            <div className={styles.categories}>
+                                <a href="/attendance/AttendanceUpdater">
+                                    <h2 className={styles.ttabs}>ATTENDANCE MANAGER</h2>
+                                </a>
+                            </div>
+                        </div>
+                        <div className={styles.tabs}>
+                            <div className={styles.categories}>
+                                <a href="/marks/MarksContainer">
+                                    <h2 className={styles.ttabs}>MARKS UPDATER</h2>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-            <div className={styles.tabs}>
-              <div className={styles.categories}>
-                <a href="/attendance/AttendanceUpdater">
-                  <h2 className={styles.ttabs}>ATTENDANCE MANAGER</h2>
-                </a>
-              </div>
-            </div>
-            <div className={styles.tabs}>
-              <div className={styles.categories}>
-                <a href="/marks/MarksContainer">
-                  <h2 className={styles.ttabs}>MARKS UPDATER</h2>
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
-  return <a href="/api/auth/signin">SIGN IN</a>;
+        );
+    }
+    return <a href="/api/auth/signin">SIGN IN</a>;
 };
 
 export default TeeacherHome;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,41 +4,50 @@ import AdminHome from './home/AdminHome';
 import Plogin from './Plogin.js';
 import { getSession } from 'next-auth/react';
 const environment = process.env.NODE_ENV;
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Home({ user }) {
-  const role = user?.role;
-  console.log(`The user is: ${JSON.stringify(user?.role)}`);
-  return (
-    <div className={styles.container}>
-      {!user && <Plogin />}
-      {user && role === 'User' && (
-        <UserHome role={role} />
-      )}
-      {user && role === 'Admin' && <AdminHome role={role} />}
-    </div>
-  );
+export default function Home({ user, time }) {
+    const role = user?.role;
+    console.log(time);
+    return (
+        <div className={styles.container}>
+            {!user && <Plogin />}
+            {user && role === 'User' && <UserHome role={role} />}
+            {user && role === 'Admin' && <AdminHome role={role} />}
+        </div>
+    );
 }
 
 export async function getServerSideProps({ req }) {
+    const session = await getSession({ req });
+    const email = session?.user.email;
 
-  const session = await getSession({ req });
-  const email = session?.user.email;
+    const url =
+        environment === 'production' ? 'https://semac.vercel.app/api' : `http://localhost:3000/api`;
 
-  const url =
-    environment === 'production' ? 'https://semac.vercel.app/api' : `http://localhost:3000/api`;
+    let user = null;
+    let time = null;
 
-  let user = null;
+    if (email) {
+        const res = await fetch(`${url}/auth/user/?email=${email}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        user = await res.json();
 
-  if (email) {
-    const res = await fetch(`${url}/auth/user/?email=${email}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+        // Pass data to the page via props
+    }
+
+    const rez = await fetch(`${url}/time`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
     });
-    user = await res.json();
+    time = await rez.json();
 
-    // Pass data to the page via props
-  }
-  return { props: { user } };
+    return { props: { user, time } };
 }
